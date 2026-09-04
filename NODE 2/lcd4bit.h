eeedd7392ee6fefd2c3fcd63be84d3837b456ece
eswarvelu <eswaramoorthyvelayutham@gmail.com>
2026-09-04 22:16:39 +0530

    Add files via upload

---
+++ NODE 2/lcd4bit.h
@@ -0,0 +1,114 @@
+#include<lpc21xx.h>
+#include "header.h"
+
+#define lcd_d 0xf<<23
+#define rs 1<<28
+#define en 1<<29
+
+void lcd_init(void);
+void lcd_command(unsigned char cmd);
+void lcd_data(unsigned char data);
+void lcd_int(int n);
+void str(unsigned char *p);
+void lcd_float(float f_val);
+
+void lcd_init(void)
+{
+IODIR1=lcd_d|rs|en;
+lcd_command(0x01);
+lcd_command(0x02);
+lcd_command(0x0c);
+lcd_command(0x28);
+}
+
+void lcd_command(unsigned char cmd)
+{
+    //unsigned char lower,higher;
+
+    IOCLR1 = lcd_d;   
+    IOSET1 = (cmd & 0xF0) << 19;
+    IOCLR1 = rs;
+    IOSET1 = en;
+    ms_delay(2);
+    IOCLR1 = en;
+
+    IOCLR1 = lcd_d;   
+    IOSET1 = (cmd & 0x0F) << 23;    
+    IOCLR1 = rs;
+    IOSET1 = en;
+    ms_delay(2);
+    IOCLR1 = en;
+}
+
+void lcd_data(unsigned char d)
+{
+   IOCLR1= lcd_d;   
+    IOSET1 = (d & 0xF0) << 19;
+    IOSET1 = rs;
+    IOSET1 = en;
+    ms_delay(2);
+    IOCLR1 = en;
+
+    IOCLR1 = lcd_d;   
+    IOSET1 = (d & 0x0F) << 23;    
+    IOSET1 = rs;
+    IOSET1 = en;
+    ms_delay(2);
+    IOCLR1 = en;
+
+    
+}
+
+void lcd_int(int n)
+{
+int i=0;
+unsigned char arr[5];
+if(n==0)
+{
+lcd_data('0');
+}
+else
+{
+if(n<0)
+{
+lcd_data('-');
+n=-n;
+}
+while(n>0)
+{
+arr[i++]=n%10;
+n/=10;
+}
+for (--i;i>=0;i--)
+{
+lcd_data(48+arr[i]);
+}
+}
+}
+
+void str(unsigned char *p)
+{
+while(*p)
+{
+lcd_data(*p);
+p++;
+}
+}
+
+void lcd_float(float f_val)
+
+{
+
+  int number;
+
+  number = f_val;
+
+  lcd_int(number);
+
+  lcd_data('.');
+
+  number=(f_val-number)*100;
+
+  lcd_int(number);	
+
+}
---
+++ NODE 2/n2main.c
@@ -0,0 +1,86 @@
+#include <lpc21xx.h>
+#include "lcd4bit.h"
+#include "header.h"
+#include "types.h"
+
+
+#define l1_led (1<<8)
+#define l2_led (1<<9)
+#define l3_led (1<<10)
+						
+int main()
+{
+    u32 value;
+    CAN2_MSG m1;
+    IODIR0 |= l1_led | l2_led | l3_led;
+    IOSET0 = l1_led | l2_led | l3_led;
+
+
+	can2_init();
+    lcd_init();
+	lcd_command(0x01);       // Clear LCD
+
+    lcd_command(0x80);       // Row 1, column 1
+    str("CAN Bus Based");
+
+    lcd_command(0xC0);       // Row 2, column 1
+    str("Automatic Water-Level");
+
+    lcd_command(0x94);       // Row 3, column 1
+    str("Gate Control System");
+
+    ms_delay(5000);          // Display title for 5 seconds
+
+    lcd_command(0x01); 
+
+    while(1)
+    {
+      can2_rx(&m1);
+	  value=m1.byteA;
+
+       if(value == 7500)
+
+        {
+
+            /* Level 1 */
+
+            IOCLR0 = l1_led;
+            IOSET0 = l2_led | l3_led;
+			lcd_command(0x01);
+			str("LEVEL1");
+
+        }
+
+
+        else if(value == 21750)
+
+        {
+
+            /* Level 2 */
+			IOCLR0 = l2_led;
+            IOSET0 = l1_led | l3_led;
+			lcd_command(0x01);
+			str("LEVEL2");
+        }
+
+        else if(value == 36000)
+
+        {
+            /* Level 3 */
+            IOCLR0 = l3_led;
+            IOSET0 = l1_led | l2_led;
+			lcd_command(0x01);
+			str("LEVEL3");
+
+        }
+
+		else
+		{
+    		lcd_command(0x01);
+    		str("ERROR");
+		}
+
+
+    }
+
+}
